4c84a1342d2b0ab329f4f985fc5f0f5a1d83bd02
7refisa <rikhsanti@gmail.com>
2025-11-02 16:17:32 +0700

    Update filter cari buku

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -26,7 +26,7 @@ const SearchBar = ({ onSearch, isLoading }) => {
           type="text"
           value={searchTerm}
           onChange={(e) => setSearchTerm(e.target.value)}
-          placeholder="Judul, Penulis, atau Subjek..."
+          placeholder="Judul atau Penulis.."
           required
           className="w-full pl-10 pr-4 py-2 rounded-md border border-pink-200 focus:outline-none focus:ring-2 focus:ring-pink-400"
         />
